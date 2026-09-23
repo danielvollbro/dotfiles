c0a9473117bf1354e810ecbf67f11aaec99585b6
J.A.R.V.I.S. <jarvis@vollbro.se>
2026-09-23 14:40:23 +0200

    fix(kanshi): add dpms on after reload in kanshi-laptop-reload

--- home/home.nix
+++ home/home.nix
@@ -11,11 +11,13 @@
     btop
     (pkgs.writeShellScriptBin "kanshi-laptop-reload" ''
       # Kanshi cannot re-enable a disabled monitor on Hyprland 0.55 lua;
-      # a hyprctl reload is required after the laptop profile applies.
+      # a hyprctl reload (disabled) + dpms on (black screen) is required.
       export HYPRLAND_INSTANCE_SIGNATURE=$(ls /run/user/1000/hypr/ | head -1)
       export XDG_RUNTIME_DIR=/run/user/1000 WAYLAND_DISPLAY=wayland-1
       sleep 1
-      exec ${pkgs.hyprland}/bin/hyprctl reload
+      ${pkgs.hyprland}/bin/hyprctl reload
+      sleep 1
+      ${pkgs.hyprland}/bin/hyprctl dispatch dpms on
     '')
   ];
 
