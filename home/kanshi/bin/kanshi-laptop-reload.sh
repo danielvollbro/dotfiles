d9e33584577f035617d41afcdcae6c8e89597cd9
J.A.R.V.I.S. <jarvis@vollbro.se>
2026-09-23 14:36:40 +0200

    refactor(kanshi): point exec at HM-installed kanshi-laptop-reload

--- home/kanshi/bin/kanshi-laptop-reload.sh
+++
@@ -1,6 +0,0 @@
-#!/run/current-system/sw/bin/sh
-# Kanshi kan inte ateraktivera en disabled monitor i Hyprland lua (0.55) - kraver hyprctl reload
-export HYPRLAND_INSTANCE_SIGNATURE=$(ls /run/user/1000/hypr/ | head -1)
-export XDG_RUNTIME_DIR=/run/user/1000 WAYLAND_DISPLAY=wayland-1
-sleep 1
-hyprctl reload
