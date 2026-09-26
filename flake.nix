5e5a436f40f6b29421ec848faedad833b7cc2f5a
Daniel Vollbro <wollbro90@gmail.com>
2026-09-26 12:44:59 +0200

    feat(nixos): restructure project

--- flake.nix
+++ flake.nix
@@ -24,20 +24,20 @@
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-          home-manager.users.daniel = import ./home/home.nix;
+          home-manager.users.daniel = import ./home/laptop.nix;
         }
       ];
     };
-    nixosConfigurations.headless-pc = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.gaming-pc = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./hosts/common/configuration.nix
-        ./hosts/headless-pc/configuration.nix
+        ./hosts/gaming-pc/configuration.nix
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-          home-manager.users.daniel = import ./home/home.nix;
+          home-manager.users.daniel = import ./home/gaming-pc.nix;
         }
       ];
     };
--- hosts/gaming-pc/configuration.nix
+++ hosts/gaming-pc/configuration.nix
@@ -1,13 +1,9 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 { config, pkgs, ... }:
 
 {
   imports = [ ./hardware-configuration.nix ];
 
-  networking.hostName = "headless-pc"; # Define your hostname.
+  networking.hostName = "gaming-pc";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -24,9 +20,6 @@
     iptables -A nixos-fw -p tcp --dport 22 -s 10.0.50.20 -j ACCEPT
     iptables -A nixos-fw -p tcp --dport 22 -s 10.0.10.16 -j ACCEPT
   '';
-
-  # Or disable the firewall altogether.
-  networking.firewall.enable = false;
 
   # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
   # and migrated your data accordingly.
